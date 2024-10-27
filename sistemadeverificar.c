a1facde4cbd40ccc24e4b975e84c5023dc129b70
Pedro Henrique Cadete <162760498+Phmaker@users.noreply.github.com>
2024-10-27 14:52:26 -0300

    Update sistemadeverificar.c

--- sistemadeverificar.c
+++ sistemadeverificar.c
@@ -1,78 +1,172 @@
 #include <stdio.h>	
 #include <stdlib.h>	
 #include <string.h> //biblioteca para comparação de string comando "strcmp" e "strcpy"
+#include <locale.h> //biblioteca responsavel para utilizar acentos 
+#include <windows.h> //biblioteca para usar a função system
 
-int main(){
+// crio uma struct a qual irá armazenar os dados do novo usuário criado.
+struct sistemaloginuser {
+	char escolhauser;
+	char iduserLogin[50];
+	char iduserSenha[50];
+	char nomeuser[20];
+	int qntdd_de_novosusers;
+};
 
-char escolhausuario[50];
-char Idusuario[50];
-char Idsenha[50];
-char novoidusuario[50];
-char novoidsenha[50];	
+// crio uma struct para armazenar os dados de cadastro de um novo cliente PF.
+struct cadastro_novo_cliente {
+	char nome[50];
+	char sobrenome[50];
+	char placa_carro[8];
+	char cpf[15];
+	char email[50];
+	char telefone[17];	
+};
 
-	printf("Alinhafacil\n\n");
+//utilizo os typedef para atribuir apelidos para as struct, (?tipo como se fosse um ponteiro?), Perguntar ao Prof. Marcos
+typedef struct sistemaloginuser sistemaloginuser;
+
+int main() {
 	
-	//Crio O laço de repetição na qual iria repetir ate o usuario cadastrar o novo usuario ou realizar login
+	// código para utilizar acentos
+	setlocale(LC_ALL, "Portuguese");
+	
+	// código para inserir cor no terminal
+	system("color 1E");
+
+	// introduzindo o apelido a um tipo de variável para utilizar scanf
+	sistemaloginuser usuario;
+	sistemaloginuser loginuser;
+	
+	// declaro uma variável do tipo inteiro para verificar se já houve usuário 
+	int usuariosCadastrados = 0;
+	int loginRealizado = 0;
+
+
+// Comando para adicionar cor as saídas como printf	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 240);
+	
+	// apenas uma "interface" para deixar o terminal mais bnt
+	printf("+============================================================================+\n\n");
+	printf("\t\t\t\tALINHA FÁCIL\n\n");
+	printf("+============================================================================+\n\n");
+	
+	// inicio o principal laço de repetição do programa
+	do {
+	
+		system("color 1E");
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 240);
+	
+		// peço ao usuário para escolher uma opção e salvo a sua escolha
+		printf("\n\t\t\tEscolha uma das opções!\n\n\n");
+		printf("\n1 - Para Logar | 2 - Para cadastrar | 3 - Sair do Programa\n\n\n$");
+		scanf(" %c", &loginuser.escolhauser);
+	
+		// comando usado para não ter problema em lixo de memória nos próximos scanf
+		fflush(stdin);
+
+		switch (loginuser.escolhauser) {
+	
+			case '1':
+				
+				//comando para limparar cmd
+                system("cls");
+                
+                //commado para colorir terminal e saida de dados
+                system("color 1E");
+                SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 240);
+                
+                //imprimir "interface" grafica
+                printf("+===================================================================+\n\n");
+                printf("\t\t\tALINHA FÁCIL\n\n");
+                printf("+===================================================================+\n\n");
+                printf("\n\t\t\tTELA DE LOGIN\n\n");
+				
+				
+				//se usuarioscadastrados for menor ou = a 0 entao 
+                if (usuariosCadastrados <= 0) {
+                    printf("\nNenhum usuário encontrado. Realize o cadastro primeiro!\n");
+                } 
+					//senao faça
+					else {
+                    printf("\nDigite Seu Login:\n\n$");
+                    scanf(" %49s", loginuser.iduserLogin);
+                    fflush(stdin);
+                    printf("\nDigite Sua Senha:\n\n$");
+                    scanf(" %49s", loginuser.iduserSenha);
+                    fflush(stdin);
+
+
+					//comparaçao entre strings
+                    if (strcmp(loginuser.iduserLogin, usuario.iduserLogin) == 0 && strcmp(loginuser.iduserSenha, usuario.iduserSenha) == 0) {
+                        printf("\nLogin Realizado com sucesso!\n");
+                        loginRealizado = 1;
+                    } else {
+                        printf("\nLogin ou senha incorretos.\n");
+                    }
+                }
+                break;
+
+			case '2':
+                do {
+                    // comando para limpar a tela do terminal
+                    system("cls");
+                    system("color 1E");
+                    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 240);
+
+                    // após a limpeza do terminal, imprimo na tela novamente essa "interface"
+                    printf("+===================================================================+\n\n");
+                    printf("\t\t\tALINHA FÁCIL\n\n");
+                    printf("+===================================================================+\n\n");
+                    printf("\n\t\t\tTELA DE CADASTRO\n\n");
+
+                    // peço ao usuário para cadastrar seu nome
+                    printf("\nDigite Seu Nome:\n\n$");
+                    scanf(" %19s", usuario.nomeuser);
+                    fflush(stdin);
+
+                    // peço ao usuário para cadastrar seu login
+                    printf("\nDigite Seu Login:\n\n$");
+                    scanf(" %49s", usuario.iduserLogin);
+                    fflush(stdin);
+
+                    // Verificar se o login já existe
+                    if (strcmp(usuario.iduserLogin, loginuser.iduserLogin) == 0) {
+                        printf("\nJá existe um usuário cadastrado com esse login. Tente novamente.\n");
+                    } 
+					else {
+                        // peço ao usuário para cadastrar sua senha
+                        printf("\nDigite Sua Senha:\n\n$");
+                        scanf(" %49s", usuario.iduserSenha);
+                        fflush(stdin);
+                        printf("\nCadastro Realizado com sucesso\n");
+                        usuariosCadastrados = 1; // Indica que um usuário foi cadastrado
+                        break;
+                    }
+                } while (1);
+                break;
+			
+			case '3':
+				system("color 1E");
+				SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 240);
+				
+				// realizo uma confirmação, pois às vezes o usuário pode clicar sem querer
+				printf("\nDeseja realmente sair do Programa?\n");
+				printf("\nDigite 1 - para confirmar\n");
+				scanf(" %s", &loginuser.escolhauser);
+				fflush(stdin);
+				system("cls");
+				// utilizando um if para ser a lógica: se o usuário confirmar, encerra o programa
+				if (loginuser.escolhauser == '1') {
+					return 0;
+				}
+				break;
+		}
+	} while (!loginRealizado); //utilizei a logica do enquanto for diferente de loginrealizado execute
+
+	printf("\nBem Vindo %s\n", usuario.nomeuser);
+	
 	do{
-		//Peço para o usuario realizar um escolha
-	printf("\nDeseja efetuar o Login ou Cadastrar um novo usuario?\n$");
-	scanf("%s", escolhausuario);
-	
-	//se o usuario escolher login executo o codigo abaixo
-		if (strcmp(escolhausuario,"Login") == 0 || strcmp(escolhausuario,"login") == 0){
-			
-			printf("Digite seu Login:\n$");
-			scanf("%s", &Idusuario);
-			printf("\nDigite sua senha:\n$");
-			scanf("%s", &Idsenha);
-			
-			
-			//se o id do usuario e sua senha for correspondentes entao imprima Bem vindo (Usuario)
-			if (strcmp(Idusuario, "admin123") == 0 && strcmp(Idsenha, "12345" ) == 0){
-				
-				printf("\nBem Vindo admin123\n\n");
-} 
+	}while(1);
 
-			//senão forem correspondentes logo imprima login ou senha incorretos
-			else{
-				
-				printf("Login ou Senha Incorretos.\n\n");
+	return 0;
 }
-}
-		
-		// se o usuario escolher cadastro executo o codigo abaixo
-		else if (strcmp(escolhausuario, "Cadastrar") == 0 || strcmp(escolhausuario, "cadastrar") == 0){
-				do{	
-				
-					strcpy(Idusuario, "admin123");
-					
-					printf("\nDigite o seu login desejado:\n$");
-					scanf("%s", &novoidusuario);
-					
-				if (strcmp(novoidusuario, Idusuario) == 0){
-					printf("\nJá Existe esse Login cadastrado! Favor tente outro\n\n");				
-
-}
-				else{
-					printf("\nDigite sua Senha desejada;\n$");
-					scanf("%s", novoidsenha);
-					printf("\nUsuario Cadastrado com Sucesso!\n\n");
-					
-					}
-	
-				}while(strcmp(novoidusuario, Idusuario) == 0);
-				
-				
-		}else {
-			printf("\n\nEscolha uma Opção existente!\n\n");
-}
-				
-		}while (strcmp(escolhausuario, "Login") != 0  && strcmp(escolhausuario, "login") != 0 && strcmp(escolhausuario, "Cadastrar") != 0 && strcmp(escolhausuario, "cadastrar") != 0);
-	
-
-system("Pause");
-
-return 0;
-	
-}
-
